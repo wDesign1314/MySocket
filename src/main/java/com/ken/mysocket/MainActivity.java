c7ae27f77cc4984dd71f9200d3f19ac52e4c228c
wDesign <android_design@163.com>
2017-07-03 10:22:04 +0800

    首次提交项目代码111

--- src/main/java/com/ken/mysocket/MainActivity.java
+++ src/main/java/com/ken/mysocket/MainActivity.java
@@ -30,6 +30,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         switch (view.getId()){
             case R.id.btn_send:
                 sendq();
+                
                 break;
         }
     }
